aa8405435d1aa0d1e8073cc4f0c0fb337b770e8f
alejandrom247 <alejandroruizmadruga@gmail.com>
2025-08-27 14:49:25 -0400

    apply state user

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,27 @@ import {QueryClientProvider,} from '@tanstack/react-query'
 import { queryClient } from './lib/queryClient'
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 import { useEffect } from 'react'
+import Cookies from 'node_modules/@types/js-cookie'
+import { getMe } from './pages/login/actions/authActions'
+import { useAuthStore } from './hooks/useAuthStore'
 
 function App() {
 
+  
+
   useEffect(()=>{
-    
+    const token = Cookies.get('access_token');
+    if(token){
+      getMe().then(user => {
+        useAuthStore.getState().setUser(user)
+      }
+      ).catch(
+        _error=>{
+          useAuthStore.getState().setUser(null);
+          console.error(_error)
+        }
+      )
+    }
   }, [])
 
   return (
